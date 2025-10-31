c390b660281f5e1c1343454a0c46d7ce3dfb0f0d
mpuellos <102886709+mpuellos@users.noreply.github.com>
2025-10-31 12:00:43 -0500

    Update javamavenDSL3.groovy

--- DSL/javamavenDSL3.groovy
+++ DSL/javamavenDSL3.groovy
@@ -1,9 +1,9 @@
 job('Java Maven App DSL 3') {
     description('Java Maven App con DSL para el curso de Jenkins')
     scm {
-        git('https://github.com/macloujulian/simple-java-maven-app.git', 'master') { node ->
-            node / gitConfigName('macloujulian')
-            node / gitConfigEmail('macloujulian@gmail.com')
+        git('https://github.com/mpuellos/simple-java-maven-app.git', 'main') { node ->
+            node / gitConfigName('mpuellos')
+            node / gitConfigEmail('mpuellosuarez@gmail.com')
         }
     }
     triggers {
@@ -49,9 +49,9 @@ job('Java Maven App DSL 3') {
 job('Job test Hola Mundo') {
 	description('Aplicacion Hola Mundo de Prueba')
 	scm {
-		git('https://github.com/macloujulian/simple-java-maven-app.git', 'master') { node ->
-		    node / gitConfigName('macloujulian')
-		    node / gitConfigEmail('macloujulian@gmail.com')
+		git('https://github.com/mpuellos/simple-java-maven-app.git', 'main') { node ->
+		    node / gitConfigName('mpuellos')
+		    node / gitConfigEmail('mpuellosuarez@gmail.com')
 		}
 	}
 	triggers {
